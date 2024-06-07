f45647d29c162d9332ae3c8869c837345eb11d1b
Harsh <harsh2020bhardwaj@gmail.com>
2024-06-07 23:54:40 +0530

    hh

--- javaimplementation/src/javaimplementation/greedy1.java
+++ javaimplementation/src/javaimplementation/greedy1.java
@@ -5,7 +5,6 @@ import java.util.*;
 public class greedy1 {
 
 	public static void main(String[] args) {
-
 		// activity selection
 		int start[] = { 1, 3, 0, 5, 8, 5 };
 		int end[] = { 2, 4, 6, 7, 9, 9 };
