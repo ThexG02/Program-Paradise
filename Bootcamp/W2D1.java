8cf65a5da470ba997a1475b5a52d0b99643f88fc
Harsh <harsh2020bhardwaj@gmail.com>
2024-06-12 12:47:25 +0530

    bootcamp

--- Bootcamp/W2D1.java
+++
@@ -1,6 +0,0 @@
-public class W2D1 {
-    public static void main(String[] args) {
-       // week 2 day 1: linked list 
-       
-    }
-}
--- javaimplementation/src/javaimplementation/stack.java
+++ javaimplementation/src/javaimplementation/stack.java
@@ -1,6 +1,9 @@
 package javaimplementation;
 import java.util.*;
+
 public class stack {
+
+ 
 public static void pushbtm(Stack<Integer> s, int data) {
 	if(s.isEmpty()) {
 		s.push(data);
@@ -10,12 +13,13 @@ public static void pushbtm(Stack<Integer> s, int data) {
 	pushbtm(s,data);
 	s.push(top);
 }
+
 	public static void main(String[] args) {
 Stack <Integer> s = new Stack<>();
 s.push(1);
 s.push(2);
 s.push(3);
-pushbtm(s,4);
+//pushbtm(s,4);
 while(!s.isEmpty()) {
 System.out.println(s.peek());
 s.pop();
