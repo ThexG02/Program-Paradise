117090d3b66118ad91789d4336024845ab309e9d
Harsh <harsh2020bhardwaj@gmail.com>
2024-06-17 01:06:44 +0530

    dsapatterns

--- DSAPATTERNS/src/MATRIX_TRAVERSAL.java
+++ DSAPATTERNS/src/MATRIX_TRAVERSAL.java
@@ -1,3 +1,67 @@
 public class MATRIX_TRAVERSAL {
 
+    //ROW WISE TRAVERSAL IN A MATRIX
+    public static void rowwisetraversal(int[][] nums){
+    for(int i=0;i<nums.length;i++){
+        for(int j=0;j<nums[i].length;j++){
+            System.out.print(nums[i][j]);
+        }
+        System.out.println();
+    }
 }
+
+
+//COLOUMN WISE TRAVERSAL IN A MATRIX
+public static void coloumnwisetraversal(int[][] nums){
+    int row=nums.length;
+    int col=nums[0].length;
+    for(int i=0;i<col;i++){
+        for(int j=0;j<row;j++){
+System.out.print(nums[j][i]);
+        }
+        System.out.println();
+    }
+}
+
+// SPRIRAL ORDER TRAVERSAL IN A MATRIX
+    public static void spiralmatrix(int[][] nums){
+        int startrow =0;
+        int endrow= nums.length-1;
+        int startcol=0;
+        int endcol=nums[0].length-1;
+        while((startrow<=endrow) && (startcol<=endcol) ){
+            //top
+            for(int j=startcol;j<=endcol;j++){
+                System.out.print(nums[startrow][j]);
+            }
+            //right
+            for(int i=startrow+1;i<=endrow;i++){
+                System.out.print(nums[i][endcol]);
+            }
+            //bottom
+            for(int j=endcol-1;j>=startcol;j--){
+                System.out.print(nums[endrow][j]);
+            }
+            //left
+            for(int i=endrow-1;i>=startrow+1;i--){
+                System.out.print(nums[i][startcol]);
+            }
+            startcol++;
+            startrow++;
+            endcol--;
+            endrow--;
+        }
+
+    }
+    public static void main(String[] args) {
+        int nums[][] = {
+            {1,2,3},
+            {8,9,4},
+            {7,6,5}};
+        //rowwisetraversal(nums);
+        //coloumnwisetraversal(nums);
+        spiralmatrix(nums);
+        }
+    
+}
+
--- DSAPATTERNS/src/TWO_POINTER.java
+++ DSAPATTERNS/src/TWO_POINTER.java
@@ -1,3 +1,24 @@
 public class TWO_POINTER {
-
+    public static boolean pairwithsum(int nums[],int target){
+        int s=0;
+        int e= nums.length-1;
+        while(s<=e){
+            int sum =nums[s]+nums[e];
+            if(sum==target){
+                return true;
+            }
+            else if(sum<target){
+                s++;
+            }
+            else{
+                e--;
+            }
+            
+        }
+        return false;
+    }
+public static void main(String[] args) {
+    int nums[]= {1,2,3,4,5};
+    System.out.println(pairwithsum(nums, 97));
+}
 }
