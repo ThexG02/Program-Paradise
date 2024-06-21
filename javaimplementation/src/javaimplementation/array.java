44e982bf198410dd40ea1fe5d130b3ff5266f67b
Harsh <harsh2020bhardwaj@gmail.com>
2024-06-21 20:31:54 +0530

    cv

--- javaimplementation/src/javaimplementation/array.java
+++ javaimplementation/src/javaimplementation/array.java
@@ -1,7 +1,7 @@
 package javaimplementation;
-
+import java.util.*;;
 public class array {
-
+	
 	public static void main(String[] args) {
 
 	}
--- javaimplementation/src/javaimplementation/stack.java
+++ javaimplementation/src/javaimplementation/stack.java
@@ -1,6 +1,7 @@
 package javaimplementation;
 import java.util.*;
 
+
 public class stack {
 
 //program to push an element into a stack at the bottom 
@@ -14,16 +15,34 @@ public static void pushbtm(Stack<Integer> s, int data) {
 	s.push(top);
 }
 
-	public static void main(String[] args) {
+//program to reverse a string using  stack
+public static String strrev(String str){
+	Stack<Character> s = new Stack<>();
+	int idx =0;
+	while (idx<str.length()) {
+		s.push(str.charAt(idx));
+		idx++;
+	}
+	StringBuilder res = new StringBuilder();
+	while (!s.isEmpty()) {
+		char curr = s.pop();
+		res.append(curr);
+	}
+	str= res.toString();
+	return str;
+}
+public static void main(String[] args) {
 Stack <Integer> s = new Stack<>();
 s.push(1);
 s.push(2);
 s.push(3);
 pushbtm(s,4);
-while(!s.isEmpty()) {
-System.out.println(s.peek());
-s.pop();
-}
+// while(!s.isEmpty()) {
+// System.out.println(s.peek());
+// s.pop();
+
+// }
+System.out.println(strrev("dog"));
 	}
 
 }
