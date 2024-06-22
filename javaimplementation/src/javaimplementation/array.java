4a9d60951566c80d93bb96840152336719ec0334
Harsh <harsh2020bhardwaj@gmail.com>
2024-06-22 22:19:22 +0530

    stack and array

--- javaimplementation/src/javaimplementation/array.java
+++ javaimplementation/src/javaimplementation/array.java
@@ -2,8 +2,122 @@ package javaimplementation;
 import java.util.*;;
 public class array {
 	
-	public static void main(String[] args) {
+//Binary search using array : ONLY APPLICABLE IF THE ARRAY IS SORTED
+public static boolean binsearch(int arr[], int itm) {
+	int s=0;
+	int e=arr.length-1;
+	while(s<=e) {
+		int mid =(s+e)/2;
+		if(arr[mid]==itm) {
+			return true;
+		}
+		if(arr[mid]<itm) {
+			s=mid+1;
+		}
+		else {
+			e=mid-1;
+		}	
+	}
+	return false;
+}
 
+//PROGRAM to revervse an array using two pointer approach O(n)
+
+public static void arrrev(int [] arr){
+	int i=0;
+	int j=arr.length-1;
+	while(i<=j){
+		int temp =arr[i];
+		arr[i]=arr[j];
+		arr[j]=temp;
+		i++;j--;
+	}
+	
+}
+
+// program to print the  all the subarray of an array
+public static void subarray(int arr[]){
+	for(int i=0;i<arr.length;i++){
+		for(int j=0;j<arr.length;j++){
+			for(int k=i;k<=j;k++){
+				System.out.print(arr[k]);
+			}
+			System.out.println();
+		}
+	}
+		
+}
+
+// program to find the Mximum sum saubarray : KADAN'S ALGORITHM ->O(n) "Maximum sum from a sub array"
+public static int KADAN(int arr[]){
+	int ms=Integer.MIN_VALUE;
+	int cs=0;
+	for(int i=0;i<arr.length;i++){
+		cs+=arr[i];
+		if(cs<0){
+			cs=0;
+		}
+		ms=Math.max(cs, ms);
+	}
+	return ms;
+}
+
+// program to implement spriral matrix traversal in a 2D array
+public static void spiralmatrix(int[][] arr){
+	int startrow=0;
+	int startcol=0;
+	int endrow=arr.length-1;
+	int endcol=arr[0].length-1;
+	while((startcol<=endcol)&&(startrow<=endrow)){
+		
+		for(int j=startcol;j<=endcol;j++){
+			System.out.print(arr[startrow][j]);
+		}
+		for(int i=startrow+1;i<=endrow;i++){
+			System.out.print(arr[i][endrow]);
+		}
+		for(int j=endrow-1;j>=startcol;j--){
+			System.out.print(arr[endrow][j]);
+		}
+		for(int i= endrow-1;i>=startrow+1;i--){
+			System.out.print(arr[i][startcol]);
+		}
+		startcol++;
+        startrow++;
+        endcol--;
+        endrow--;
+	}
+}
+
+//program to diagonal sum  in a 2D array: primary diagonal +secondary diagonal
+//note : the time comlexity for this code is to O(n)
+public static int diasum(int[][] arr){
+	int sum=0;
+	for(int  i=0;i<arr.length;i++){
+		sum+=arr[i][i];
+	
+	if(i!=arr.length-i-1){
+		sum+=arr[i][arr.length-i-1];
+	}}
+	return sum;
+}
+
+	public static void main(String[] args) {
+		int[] arr ={1,-2,3,-4,-5,6};
+		int key =14;
+		//System.out.println(binsearch(arr, key));
+		// arrrev(arr);
+		// for(int i=0;i<arr.length;i++){
+		// 	System.out.print(arr[i]+" ");
+		// }
+		//subarray(arr);
+		//System.err.println(KADAN(arr));
+		int [][] arrl={{1,2,3},
+					   {8,9,4},
+				       {7,6,5}
+						};
+		//spiralmatrix(arrl);
+		System.out.println(diasum(arrl));
 	}
 
 }
--- javaimplementation/src/javaimplementation/stack.java
+++ javaimplementation/src/javaimplementation/stack.java
@@ -15,6 +15,18 @@ public static void pushbtm(Stack<Integer> s, int data) {
 	s.push(top);
 }
 
+/*program to reverse a stack
+data type integer
+*/ 
+
+public static void revstack(Stack<Integer> s){
+if(s.isEmpty()){
+return ;
+}
+int top =s.pop();
+revstack(s);
+pushbtm(s, top);
+}
 //program to reverse a string using  stack
 public static String strrev(String str){
 	Stack<Character> s = new Stack<>();
@@ -31,18 +43,107 @@ public static String strrev(String str){
 	str= res.toString();
 	return str;
 }
+
+// STOCK SPAN PROBLEM
+public static void stockspan(int[] stocks, int[]span){
+	Stack<Integer> s = new Stack<>();
+	span[0]=1;
+	s.push(0);
+	for(int i=1; i<span.length;i++){
+		int currprice =stocks[i];
+		while(!s.empty()&& currprice>stocks[s.peek()]){
+			s.pop();
+		}
+		if(s.isEmpty()){
+			span[i]=i+1;  // for the last element
+		}
+		else{
+			int prevhigh=s.peek();
+			span[i]= i-prevhigh;
+		}
+		s.push(i);
+	}
+
+}
+//program to find the next greater element in the right side using stack
+public static void nextgtr(int[] arr, int[] nextgtr){
+	Stack<Integer> s= new Stack<>();   //new stack 
+	  
+	for(int i=arr.length-1;i>=0;i--){ /*  loop starts from end to begining because  it will have a track of all the right hand 
+										   side values of the next greater*/
+		while(!s.isEmpty() && arr[s.peek()]<=arr[i]){ //compare top of stack and the current ith index value of array
+			s.pop();
+		}
+		if(s.isEmpty()){
+			nextgtr[i]=-1;				// for the last element of the array
+		}
+		else{
+			nextgtr[i]=arr[s.peek()];			/*assigning the value of the next greater element
+												 for the ith elem5t in the array*/
+		}
+		s.push(i);								//pushing the ith element index to the stack
+
+	}
+	for(int i=0;i<nextgtr.length;i++){
+		System.out.print(nextgtr[i]+" ");			// printing the nextgreater array
+	}
+}
+
+// program for the valin parinthesis using stack
+public static boolean validparenthesis(String str){
+	Stack<Character> s= new Stack<>();
+	for(int i=0;i<str.length();i++){
+		Character ch =str.charAt(i);
+		//opening
+		if(ch=='['|| ch=='('||ch=='{'){
+			s.push(ch);
+		}
+		else{
+			//closing
+			if(s.isEmpty()){
+				return false;
+			}
+			if((ch==']'&&s.peek()=='[')||(ch=='}'&&s.peek()=='{')||(ch==')'&&s.peek()=='(')){
+				s.pop();
+			}
+			else{
+				return false;
+			}	
+		}}
+		//to check the string valid with help of stack 
+	if(s.isEmpty()){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
 public static void main(String[] args) {
 Stack <Integer> s = new Stack<>();
 s.push(1);
 s.push(2);
 s.push(3);
-pushbtm(s,4);
+//pushbtm(s,4);
 // while(!s.isEmpty()) {
 // System.out.println(s.peek());
 // s.pop();
 
 // }
-System.out.println(strrev("dog"));
-	}
-
+//System.out.println(strrev("dog"));
+// System.out.println(s);
+// revstack(s);
+// System.out.println(s);
+// int stock[]={100,80,60,70,60,85,100};
+// int span[] = new int[stock.length];
+// stockspan(stock, span);
+// for(int i =0; i<span.length;i++){
+// 	System.out.print(span[i]);
+// }
+// }
+// int []arr ={5,8,0,1,10,7};
+// int gtr[]= new int[arr.length]; 
+// nextgtr(arr, gtr);
+String str ="({[()]}())";
+System.out.println(validparenthesis(str));
+}
 }
